2d1d7c51f0bb8f17f9d3bf6cf5a3aa5e097cf27c
Akshay <akshay.parate6@gmail.com>
2022-10-10 11:23:07 +0300

    Changes reverted

--- BOT-Dashboard/src/app/app.module.ts
+++ BOT-Dashboard/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDashboardComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
